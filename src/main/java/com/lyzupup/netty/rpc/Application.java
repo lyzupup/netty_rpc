b7a79b0f96d153d7bb985bae05385caffb077e3f
lyzupup <lyznixi@163.com>
2020-05-18 00:06:03 +0800

    spingboot环境搭建

---
+++ src/main/java/com/lyzupup/netty/rpc/Application.java
@@ -0,0 +1,32 @@
+package com.lyzupup.netty.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @program: netty_rpc
+ * @description: SpringbootApplicationClass
+ * @author: xiaomanixi 小码blog
+ * @create: 2020-05-17 23:38
+ **/
+@Controller
+@SpringBootApplication
+@Configuration
+public class Application {
+
+    private static final String HELLO_SPRINGBOOT = "hello springboot!";
+
+    @RequestMapping("springboot")
+    @ResponseBody
+    public String helloSpringBoot(){
+        return HELLO_SPRINGBOOT;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
